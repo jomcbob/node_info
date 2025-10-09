23dd4d6ff389ebef3559ab05aa2c5f5ae3d8fb33
Bob <andrew@risertech.com>
2025-10-08 18:09:14 -0600

    feat: renamed files to better fit contents and added basic styling

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
-http.createServer(function (req, res) {
-  const url = req.url;
-  let filePath = '';
+http.createServer((req, res) => {
 
-  if (url === '/') {
-    filePath = './index.html';
-  } else if (url === '/about') {
-    filePath = './about.html';
-  } else if (url === '/contact') {
-    filePath = './contact.html';
-  } else {
-    filePath = './404.html';
+    if (req.url.endsWith('.css')) {
+    const cssPath = path.join(__dirname, req.url);
+    fs.readFile(cssPath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        return res.end('CSS file not found');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.end(data);
+    });
+    return;
   }
 
-  fs.readFile(filePath, (err, data) => {
+  let filePath = '';
+  if (req.url === '/') filePath = './index.html';
+  else if (req.url === '/about') filePath = './about.html';
+  else if (req.url === '/contact') filePath = './contact.html';
+  else filePath = './404.html';
+
+  fs.readFile('./header.html', 'utf8', (err, headerData) => {
     if (err) {
       res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Error loading file.');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
+      return res.end('Error loading header.');
     }
+
+    fs.readFile(filePath, 'utf8', (err, pageData) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        return res.end('Error loading page.');
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      // write header first, then page body
+      res.write(headerData);
+      res.end(pageData);
+    });
   });
-}).listen(8080, function () {
-  console.log("server start at port 8080");
-});
+}).listen(8080, () => console.log('Server running on http://localhost:8080'));
