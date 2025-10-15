157521ed79acd391cd98d0e25780abe3eb8fe657
Bob <andrew@risertech.com>
2025-10-15 09:14:39 -0600

    feat: updated app to use express and ejs templating

--- index.js
+++ index.js
@@ -1,44 +1,73 @@
-const http = require('http');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const express = require("express");
+const app = express();
 
-http.createServer((req, res) => {
-  if (req.url.endsWith('.css')) {
-    const cssPath = path.join(__dirname, req.url);
-    fs.readFile(cssPath, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        return res.end('CSS file not found');
-      }
-      res.writeHead(200, { 'Content-Type': 'text/css' });
-      res.end(data);
-    });
-    return;
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
   }
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
 
-  let filePath = '';
-  if (req.url === '/') filePath = path.join(__dirname, 'index.html');
-  else if (req.url === '/about') filePath = path.join(__dirname, 'about.html');
-  else if (req.url === '/contact') filePath = path.join(__dirname, 'contact.html');
-  else filePath = path.join(__dirname, '404.html');
+app.set("view engine", "ejs");
 
-  fs.readFile(filePath, 'utf8', (err, pageData) => {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      return res.end('Error loading page');
-    }
+app.use(express.static(path.join(__dirname, "public")));
 
-    fs.readFile(path.join(__dirname, 'header.html'), 'utf8', (err, headerData) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        return res.end('Error loading header');
-      }
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname, "views/index.ejs"));
+});
 
-      const finalHtml = pageData.replace('<body>', `<body>\n${headerData}`);
+app.get("/contact", (req, res) => {
+  res.render(path.join(__dirname, "views/contact.ejs"));
+});
 
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(finalHtml);
-    });
-  });
+app.get("/about", (req, res) => {
+  res.render(path.join(__dirname, "views/about.ejs"));
+});
 
-}).listen(8080, () => console.log('Server running on http://localhost:8080'));
+app.get('/{*splat}', (req, res) => {
+  res.status(404).render(path.join(__dirname, 'views/404.ejs'));
+});
+
+// http.createServer((req, res) => {
+//   if (req.url.endsWith('.css')) {
+//     const cssPath = path.join(__dirname, req.url);
+//     fs.readFile(cssPath, 'utf8', (err, data) => {
+//       if (err) {
+//         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//         return res.end('CSS file not found');
+//       }
+//       res.writeHead(200, { 'Content-Type': 'text/css' });
+//       res.end(data);
+//     });
+//     return;
+//   }
+
+//   let filePath = '';
+//   if (req.url === '/') filePath = path.join(__dirname, 'index.html');
+//   else if (req.url === '/about') filePath = path.join(__dirname, 'about.html');
+//   else if (req.url === '/contact') filePath = path.join(__dirname, 'contact.html');
+//   else filePath = path.join(__dirname, '404.html');
+
+//   fs.readFile(filePath, 'utf8', (err, pageData) => {
+//     if (err) {
+//       res.writeHead(500, { 'Content-Type': 'text/plain' });
+//       return res.end('Error loading page');
+//     }
+
+//     fs.readFile(path.join(__dirname, 'header.html'), 'utf8', (err, headerData) => {
+//       if (err) {
+//         res.writeHead(500, { 'Content-Type': 'text/plain' });
+//         return res.end('Error loading header');
+//       }
+
+//       const finalHtml = pageData.replace('<body>', `<body>\n${headerData}`);
+
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(finalHtml);
+//     });
+//   });
+
+// }).listen(8080, () => console.log('Server running on http://localhost:8080'));
